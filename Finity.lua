4a24077b12b17744bf0b581e49d186587eb7a3cd
TriStar <83173209+TriStarOfficial@users.noreply.github.com>
2021-06-20 20:50:08 +0530

    Update Finity.lua

--- Finity.lua
+++ Finity.lua
@@ -70,7 +70,7 @@ finity.dark_theme = { -- dark
 	checkbox_outer = Color3.fromRGB(84, 81, 86),
 	checkbox_inner = Color3.fromRGB(132, 132, 136),
 
-	slider_color = Color3.fromRGB(177, 177, 177),
+	slider_color = Color3.fromRGB(222, 53, 149),
 	slider_color_sliding = Color3.fromRGB(222, 53, 149),
 	slider_background = Color3.fromRGB(88, 84, 90),
 	slider_text = Color3.fromRGB(177, 177, 177),
